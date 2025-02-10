32895115352de3fb7b2f597272818993e5da9a83
suite <gr8bitmail@gmail.com>
2025-02-10 15:40:41 -0700

    remove unused constant

--- programs/amm/src/lib.rs
+++ programs/amm/src/lib.rs
@@ -1,4 +1,3 @@
-pub mod constants;
 pub mod errors;
 pub mod instructions;
 pub mod state;
@@ -6,7 +5,6 @@ pub mod helpers;
 
 use anchor_lang::prelude::*;
 
-pub use constants::*;
 pub use instructions::*;
 pub use state::*;
 
